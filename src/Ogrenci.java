a79eadede36f63962af41655c26458c9fe638d27
Casper <helinzere@gmail.com>
2022-07-06 14:06:29 +0300

    Ogrenci classı dördüncü bölüm yapildi.

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,7 +1,6 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba Helin");
-        System.out.println("Merhaba Taha");
-        System.out.println("Merhaba Dunya");
+
+        System.out.println("MERHABA");
     }
 }
